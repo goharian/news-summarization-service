aee7b4cd4cf0259df8861c4166265821254dc221
goharian <matanya.goharian@gmail.com>
2025-11-10 11:01:04 +0200

    Create Article models and migrate it

--- news_summarizer/articles/models.py
+++ news_summarizer/articles/models.py
@@ -1,3 +1,21 @@
+"""
+Database models.
+"""
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    """Article object."""
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField(null=True, blank=True)
+    url = models.URLField(max_length=2000)
+    published_date = models.DateTimeField()
+    source = models.CharField(max_length=255)
+
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
--- news_summarizer/articles/views.py
+++ news_summarizer/articles/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+class ArticleListCreateView(generics.ListCreateAPIView):
+    queryset = Article.objects.all().order_by('-publishedDate')
+    serializer_class = ArticleSerializer
