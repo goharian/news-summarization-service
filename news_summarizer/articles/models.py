e87e94d356eb9bc6a211da57e94eb92d8581ac7d
goharian <matanya.goharian@gmail.com>
2025-11-28 04:31:59 +0200

    Refactor Article model to include validators for title and content fields, and update docstrings for clarity

--- news_summarizer/articles/models.py
+++ news_summarizer/articles/models.py
@@ -1,21 +1,35 @@
 """
-Database models.
+Article model for storing news articles.
 """
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Article(models.Model):
     """Article object."""
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=512)
-    content = models.TextField(null=True, blank=True)
+    title = models.CharField(
+        max_length=512,
+        validators=[MinLengthValidator(5, "Title must be at least 5 characters long.")]
+    )
+    content = models.TextField(
+        null=True, 
+        blank=True,
+        validators=[MinLengthValidator(20, "Content must be at least 20 characters long.")]
+    )
     url = models.URLField(unique=True,max_length=2000)
     published_date = models.DateTimeField()
     source = models.CharField(max_length=255)
 
 
     class Meta:
+        """
+        Meta data for Article model.
+        """
         ordering = ["-published_date"]
 
     def __str__(self):
+        """
+        String representation of the Article object.
+        """
         return self.title
